35979399c72bdf730e51779cab8dbe3002c5eb54
Max Bachhuber <max.bachhuber@bahuma.io>
2025-02-10 12:21:14 +0100

    Fix player list bug

--- src/MinecraftService.php
+++ src/MinecraftService.php
@@ -157,7 +157,7 @@ class MinecraftService
             $this->query->Connect($serverAddress, $serverPort);
             $players = $this->query->getPlayers();
 
-            return new MinecraftStatus(MinecraftState::AVAILABLE, $players ? count($players) : 0, $players);
+            return new MinecraftStatus(MinecraftState::AVAILABLE, $players ? count($players) : 0, $players ?: []);
         } catch (MinecraftQueryException $e) {
             $this->logger->warning('Error getting MinecraftStatus: ' . $e->getMessage(), ['exception' => $e]);
             return new MinecraftStatus(MinecraftState::UNAVAILABLE, 0, []);
